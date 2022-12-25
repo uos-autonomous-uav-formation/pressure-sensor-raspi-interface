9b1981da0f19e16ead6a741c26599d684367d62a
Alvaro Sierra Castro <alvarosierracastro@hotmail.com>
2022-12-25 19:15:32 +0000

    Reprograme code structure

---
+++ src/aoa_sensor.py
@@ -0,0 +1,52 @@
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from .pressure_sensor import PressureSensor
+
+
+class AoaSensor:
+    
+    def __init__(self, spi, chip_select, ref_voltage, 
+    orientation: dict[int, int] = None):
+        """
+        Angle of attack sensor implementation using MCP3008.
+
+        Args:
+            spi: The SPI bus connecting to the MCP3008 chip.
+            chip_select: The chipselect pin. For example board.D7
+            ref_voltage: The refernce voltage the chip is connected to
+            orientaion: The pin alloction of each probe hole. Key is the probe hole based on image, value is the pin (min 0, max 7)  Defaults to: {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
+        
+        """
+
+        if orientation is None:
+            orientation = {
+                1: 0,
+                2: 1,
+                3: 2,
+                4: 3,
+                5: 4
+            }
+        else:
+            if max(orientation.values()) > 8:
+                raise ValueError("Pin value too high for an MCP3008 chip, maximum value 7")
+            if min(orientation.values()) < 0:
+                raise ValueError("Pin value too low for an MCP3008 chip, minimum value 0")
+
+        self._chip_select = digitalio.DigitalInOut(chip_select)
+        self._mcp = MCP.MCP3008(spi, self._chip_select, ref_voltage=ref_voltage)
+        self.ref_voltage = ref_voltage
+        self._allocate_pressure_sensor(orientation)
+
+    def _allocate_pressure_sensor(self, orientation):
+        self._pressure_sensors = {}
+        
+        for key, item in orientation.items():
+            self._pressure_sensors[key] = PressureSensor(self._mcp, item, ref_voltage=self.ref_voltage)
+
+    def dbg(self):
+        for key, item in self._pressure_sensors.items():
+            print(f"Probe location {key}, Channel {item.channel}, pressure sensor: {item.pressure} Voltage {item.voltage}")
+        
+        print("\n\n\n")
+
--- src/pressure_sensor.py
+++ src/pressure_sensor.py
@@ -1,18 +1,40 @@
-import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import adafruit_mcp3xxx.mcp3008 as MCP
+from numbers import Number
 
 # Implement a zeroing in startup (like with the airspeed sensor)
 
 class PressureSensor:
+    _min_voltage_check: float = 0.3 # When performing start up safety checks, 
+    
     zero: int = None
 
-    def __init__(self, mcp, channel, ref_voltage):
-        self.input = AnalogIn(mcp, channel)
+    def __init__(self, mcp: MCP.MCP3008, channel: int, ref_voltage: Number):
+        """ Interface for a MPXV7002DP pressure sensor via a MCP analogue to digital converter.
+        
+        """
 
+        # Initialize connection
+        self._input = AnalogIn(mcp, channel)
+        self.channel = channel
+
+        # Perform start up checks
+        self._start_up_safety_checks()
+
+        # Peform zeroing
+
+
+    def _start_up_safety_checks(self):
+        if self.voltage < self._min_voltage_check:
+            raise ValueError(f"Voltage too low for {self.channel}. Check if sensor working and wiring connected.")
     
     @property
     def pressure(self):
-        return (self.input / (5 * 0.2)) - 0.5
+        return (self._input.voltage / (5 * 0.2)) - 0.5
+
+    @property
+    def voltage(self):
+        return self._input.voltage
 
     @property
     def theoretical_error(self):
